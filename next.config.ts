321f513bc6843e7ca40b6f8578dbfa292c31ab76
krativeweb <krativewebdeveloper@gmail.com>
2026-01-18 00:16:42 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
 
-  // ✅ MUST be set for subfolder hosting
-  basePath: isProd ? "/museum" : "",
-
-  assetPrefix: isProd ? "/museum/" : "",
+  basePath: "/museum",
+  assetPrefix: "/museum/",
 
   images: {
     unoptimized: true,
-    remotePatterns: !isProd
-      ? [
-          {
-            protocol: "https",
-            hostname: "i.pinimg.com",
-          },
-          {
-            protocol: "https",
-            hostname: "cdn.prod.website-files.com",
-          },
-        ]
-      : [],
   },
 };
 
